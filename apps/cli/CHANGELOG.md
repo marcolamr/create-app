671861b8dfabb35b38007a8a59974984b798adc5
Marcos Roberto <marcola.mr@gmail.com>
2026-05-27 17:22:10 -0300

    refactor: fix a minor bug

--- apps/cli/CHANGELOG.md
+++ apps/cli/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @madda/create-app
 
+## 1.0.2
+
+### Patch Changes
+
+- Fixes a minor bug
+
 ## 1.0.1
 
 ### Patch Changes
--- apps/cli/src/helpers/boilerplate.ts
+++ apps/cli/src/helpers/boilerplate.ts
@@ -8,23 +8,23 @@ type SelectBoilerplateProps = Required<Pick<InstallerOptions, 'packages' | 'proj
 
 // This generates the _app.tsx file that is used to render the app
 export const selectAppFile = ({ projectDir, packages }: SelectBoilerplateProps) => {
-  const appFileDir = path.join(PKG_ROOT, 'template/extras/src/pages/_app');
+  const indexFileDir = path.join(PKG_ROOT, 'template/extras/src/app/page');
 
   const usingTw = packages.tailwind.inUse;
   const usingBetterAuth = packages.betterAuth.inUse;
 
-  let appFile = 'base.tsx';
+  let indexFile = 'base.tsx';
   if (usingBetterAuth && usingTw) {
-    appFile = 'with-better-auth-tw.tsx';
+    indexFile = 'with-better-auth-tw.tsx';
   } else if (usingBetterAuth && !usingTw) {
-    appFile = 'with-better-auth.tsx';
+    indexFile = 'with-better-auth.tsx';
   } else if (usingTw) {
-    appFile = 'with-tw.tsx';
+    indexFile = 'with-tw.tsx';
   }
 
-  const appSrc = path.join(appFileDir, appFile);
-  const appDest = path.join(projectDir, 'src/pages/_app.tsx');
-  fs.copySync(appSrc, appDest);
+  const indexSrc = path.join(indexFileDir, indexFile);
+  const indexDest = path.join(projectDir, 'src/app/page.tsx');
+  fs.copySync(indexSrc, indexDest);
 };
 
 // Similar to _app, but for app router
@@ -57,26 +57,5 @@ export const selectIndexFile = ({ projectDir, packages }: SelectBoilerplateProps
 
   const indexSrc = path.join(indexFileDir, indexFile);
   const indexDest = path.join(projectDir, 'src/pages/index.tsx');
-  fs.copySync(indexSrc, indexDest);
-};
-
-// Similar to index, but for app router
-export const selectPageFile = ({ projectDir, packages }: SelectBoilerplateProps) => {
-  const indexFileDir = path.join(PKG_ROOT, 'template/extras/src/app/page');
-
-  const usingTw = packages.tailwind.inUse;
-  const usingBetterAuth = packages.betterAuth.inUse;
-
-  let indexFile = 'base.tsx';
-  if (usingBetterAuth && usingTw) {
-    indexFile = 'with-better-auth-tw.tsx';
-  } else if (usingBetterAuth && !usingTw) {
-    indexFile = 'with-better-auth.tsx';
-  } else if (usingTw) {
-    indexFile = 'with-tw.tsx';
-  }
-
-  const indexSrc = path.join(indexFileDir, indexFile);
-  const indexDest = path.join(projectDir, 'src/app/page.tsx');
   fs.copySync(indexSrc, indexDest);
 };
